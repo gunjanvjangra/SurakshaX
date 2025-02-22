8a6561814308fc53b25540b81f8862969ba3b9b2
Gunjan Vaishnavi Jangra <124075942+gunjanvjangra@users.noreply.github.com>
2025-02-22 19:14:29 +0530

    Create assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+import icon from "./icon.png";
+import menu from "./menu_icon.svg";
+import cross from "./cross_icon.svg";
+import output from "./output.jpg";
+
+export const assets = {
+  icon,
+  menu,
+  cross,
+  output,
+};
