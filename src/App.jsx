8d083ad4fd57624142a42b1dec5085935462e3b9
Gunjan Vaishnavi Jangra <124075942+gunjanvjangra@users.noreply.github.com>
2025-02-22 19:17:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./comp/Header";
+
+const App = () => {
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
